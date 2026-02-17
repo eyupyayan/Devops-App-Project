7ba672a201ec6bd417d9ccce384a459b9fc5d5b8
eyupyayan <h672643@stud.hvl.no>
2026-02-17 10:08:41 +0100

    add prometheus metrics + servicemonitor

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from .routes import router
 
-app = FastAPI(title="devops-app", version="1.0.0")
+from prometheus_fastapi_instrumentator import Instrumentator
+
+app = FastAPI(title="devops-app", version="1.1.0")
 app.include_router(router)
+
+# Expose /metrics + default HTTP metrics
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
